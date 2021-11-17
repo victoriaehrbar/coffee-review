9a26dc647c1a493a89822d2b885b4c606a53edb2
github username <72711695+victoriaehrbar@users.noreply.github.com>
2021-11-17 14:12:24 -0600

    reviewed coffee

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :reviews
-    has_many :coffees, through :reviews
+    has_many :reviewed_coffees, through :reviews, source: :coffee
 
     has_many :coffees
 end
