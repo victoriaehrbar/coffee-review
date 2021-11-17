4d6de93d3fb91a8e36a834a5b6c360f304713683
github username <72711695+victoriaehrbar@users.noreply.github.com>
2021-11-17 14:45:40 -0600

    create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,17 @@ class UsersController < ApplicationController
     end
 
     def create
-    end
+        @user = User.new(user_params)
+        if @user.save
+          session[:user_id] = @user.id
+          redirect_to user_path(@user)
+        else
+          render :new
+        end
+      end
 
+      private
+      def user_params
+        params.require(:user).permit(:username, :password)
+      end
 end
