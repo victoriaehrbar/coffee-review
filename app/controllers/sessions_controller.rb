d9cf601cec8e79a088ac53e95dc539d6ef45ec7f
github username <72711695+victoriaehrbar@users.noreply.github.com>
2021-11-17 21:33:42 -0600

    starting create method in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,4 +7,12 @@ class SessionsController < ApplicationController
         session.delete(:user_id)
         redirect_to '/'
     end
+
+    def new
+    end
+
+    def create
+        User.find_by(username :params[:user][:username])
+        if @user && @user.authenticate(password: params[:user][:password])
+    end
 end
