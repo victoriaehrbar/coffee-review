90ac3946877ad36ff4d55e928e647c32436380dd
github username <72711695+victoriaehrbar@users.noreply.github.com>
2021-11-18 12:52:51 -0600

    more create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,5 +14,13 @@ class SessionsController < ApplicationController
     def create
         User.find_by(username :params[:user][:username])
         # if @user && @user.authenticate(password: params[:user][:password])
+
+        if @user.try(:authenticate, params[:user][:password])
+            session[:user_id] = @user.user_id
+            redirect_to user_path(@user)
+        else
+            flash[:error] = "Incorrect login info"
+            redirect_to login_path
+        end
     end
 end
