6ee57fb740f6798c5978a18b4548ca688a45161e
github username <72711695+victoriaehrbar@users.noreply.github.com>
2021-11-18 19:54:19 -0600

    typo

--- app/controllers/coffees_controller.rb
+++ app/controllers/coffees_controller.rb
@@ -4,4 +4,10 @@ class CoffeesController < ApplicationController
         @coffee = Coffee.new
         @coffee.build_brand
     end
+
+    private
+
+    def coffee_params
+      params.require(:coffee).permit(:name, :roast_type, :description, :brand_id, brand_attributes: [:name])
+    end
 end
