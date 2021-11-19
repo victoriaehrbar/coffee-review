b596639d0ee5a7c5a2e50204433622078c2c76d9
github username <72711695+victoriaehrbar@users.noreply.github.com>
2021-11-19 14:38:14 -0600

    validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,10 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :coffee
+
+  validates :title, presence: true
+  validates :score, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10}
+
+
+  validates :coffee, uniqueness: { scope: :user, message: "You have already reviewed this coffee."  }
 end
