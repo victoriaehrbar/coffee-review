a1e0f6b801b06336a74734eac13a9c5d3aca42c7
github username <72711695+victoriaehrbar@users.noreply.github.com>
2021-11-19 11:43:47 -0600

    new method

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,12 @@
 class ReviewsController < ApplicationController
 
     def new
-    end
+        if @coffee = Coffee.find_by_id(params[:coffee_id])
+          @review = @coffee.reviews.build
+        else
+          @review = Review.new
+        end
+      end
 
     def index
     end
