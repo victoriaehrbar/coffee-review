fe50f2c6503d1973219d4b0e8f5203525baf4318
github username <72711695+victoriaehrbar@users.noreply.github.com>
2021-11-20 13:11:09 -0600

    omniauth controller methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -23,4 +23,18 @@ class SessionsController < ApplicationController
             redirect_to login_path
         end
     end
+
+
+def omniauth
+    @user = User.create_by_google_omniauth(auth)
+
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
+  end
+
+  private
+
+    def auth
+      request.env['omniauth.auth']
+    end
 end
