87987e59b57cc72c189d3d67bf8265425f5ccbe5
github username <72711695+victoriaehrbar@users.noreply.github.com>
2021-11-20 12:41:16 -0600

    navbar issues

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
-end
+
+    def render_nav_bar
+      if logged_in?
+        render partial: 'layouts/loggedin'
+      else
+        render partial: 'layouts/loggedout'
+      end
+    end
+  end
