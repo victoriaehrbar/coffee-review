a504750ccb5a25677f0626964caedc7b67f0c37e
github username <72711695+victoriaehrbar@users.noreply.github.com>
2021-11-20 11:13:18 -0600

    review helper method

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,10 @@
 module ReviewsHelper
-end
+
+    def display_header(review)
+      if params[:coffee_id]
+          content_tag(:h1, "Add a review for #{review.coffee.name} -  #{review.coffee.brand_name}")
+      else
+        content_tag(:h1, "Create review")
+      end
+    end
+  end
