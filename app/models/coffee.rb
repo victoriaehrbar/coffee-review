e8f50f8c66ce01228e33eecda51c991d1c22585a
github username <72711695+victoriaehrbar@users.noreply.github.com>
2021-11-19 18:03:33 -0600

    coffee model

--- app/models/coffee.rb
+++ app/models/coffee.rb
@@ -4,4 +4,9 @@ class Coffee < ApplicationRecord
   has_many :reviews
   has_many :users, through: :reviews
   accepts_nested_attributes_for :brand
+
+  def brand_attributes=(attributes)
+    self.brand = Brand.find_or_create_by(attributes) if !attributes['brand_name'].empty?
+    self.brand
+  end
 end
