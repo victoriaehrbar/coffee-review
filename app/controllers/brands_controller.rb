a4a0e5a58732be876ce9bfb929c59d5fb7fc3a20
github username <72711695+victoriaehrbar@users.noreply.github.com>
2021-11-20 19:25:42 -0600

    helpers

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,4 +1,5 @@
 class BrandsController < ApplicationController
+    before_action :redirect_if_not_logged_in
 
     def index
         @brands = Brand.all
--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -2,7 +2,7 @@ module ReviewsHelper
 
     def display_header(review)
       if params[:coffee_id]
-          content_tag(:h1, "Add a review for #{review.coffee.name} -  #{review.coffee.brand_name}")
+          content_tag(:h1, "Add a review for #{review.coffee.name} -  #{review.coffee.brand.brand_name}")
       else
         content_tag(:h1, "Create review")
       end
