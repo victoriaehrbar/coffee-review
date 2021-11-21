84d250d20c9c7f09b26ce98e4374904fd47c9f8f
github username <72711695+victoriaehrbar@users.noreply.github.com>
2021-11-21 09:33:51 -0600

    email added

--- db/schema.rb
+++ db/schema.rb
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2021_11_17_195300) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
