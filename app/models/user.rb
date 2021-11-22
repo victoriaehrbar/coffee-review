ad6d3705e57b11828c8b4933ba96b52158bb9dd3
github username <72711695+victoriaehrbar@users.noreply.github.com>
2021-11-22 14:08:34 -0600

    removed github

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
     has_secure_password
 
     def self.from_omniauth(auth)
+      byebug
       self.find_or_create_by(username: auth[:info][:email]) do |u|
         u.password = SecureRandom.hex
       end
